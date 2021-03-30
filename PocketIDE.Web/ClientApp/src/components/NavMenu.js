94a8e085ff87169770908cdbc53780ed516985b2
Cosmin <cosmin.aftanase@yahoo.com>
2021-03-31 01:25:08 +0300

    test

--- PocketIDE.Web/ClientApp/src/components/NavMenu.js
+++ PocketIDE.Web/ClientApp/src/components/NavMenu.js
@@ -23,21 +23,21 @@ export class NavMenu extends Component {
 
   render () {
     return (
-      <header>
+      <header className="bg-dark">
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">PocketIDE.Web</NavbarBrand>
+            <NavbarBrand tag={Link} to="/" className="text-white">PocketIDE</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-white" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                  <NavLink tag={Link} className="text-white" to="/counter">Counter</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-white" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
